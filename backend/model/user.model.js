c91692825ae0918d47836f13f7ad82df3f45ff37
mohitSinghXd <mohitsingh.builds@gmail.com>
2026-04-01 21:46:22 +0530

    prehook added

--- backend/model/user.model.js
+++ backend/model/user.model.js
@@ -1,4 +1,5 @@
-import mongoose from "mongoose"; 
+import mongoose from "mongoose";  
+import bcrypt from "bcrypt"
 const userSchema = new mongoose.Schema({
     username :{
         type:String  , 
@@ -27,7 +28,12 @@ const userSchema = new mongoose.Schema({
     prompt :[{ 
         type: String 
     }]
-}) 
+})  
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    this.password = bcrypt.hash(this.password, 10);
+    next();
+});
 
 const User = mongoose.model("users" , userSchema)  ;  
 
